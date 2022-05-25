4fc8c9503b058454c2db7ff4b51ba5914603566e
kammachat <kammachat.k@ku.th>
2022-05-25 14:02:00 +0700

    return JSON Suggestion

--- app/Http/Controllers/Api/OcrController.php
+++ app/Http/Controllers/Api/OcrController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Container;
+use App\Models\MasterIso;
 use Illuminate\Http\Request;
 use thiagoalessio\TesseractOCR\TesseractOCR;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Http;
 use Eastwest\Json\Facades\Json;
 use Illuminate\Http\File;
+
+use function PHPUnit\Framework\isType;
 
 class OcrController extends Controller
 {
@@ -39,52 +42,68 @@ class OcrController extends Controller
         $container = new Container();
         $container->path = $path;
 
-        $regex_iso = "/[0-9]{2}[A-Z][0-9]/";
-        $check_con = 0;
-        $check_iso = 0;
+        $regex_iso = "/[BMPL][0-9]|[0-9]{2}[A-Z0-9][0-9]|[PESIRT]/";
         $regex_con_num = "/[A-Z]{4}[0-9]{7}/";
-        $con_num = "";
+
+        $suggester_iso = array();
 
         try {
             $ocr = new TesseractOCR();
             $ocr->image($file);
             $scanned = ($ocr->run());
+            $scanned = preg_replace("/[^A-Z0-9 ]+/i", "", $scanned);
             $scanned = preg_replace("/\s+/", "", $scanned); // cut all whitespace
-            print($scanned . "\n");
-            if (preg_match($regex_con_num, $scanned, $match) && $check_con == 0) {
-                foreach ($match as $key => $value) {
-                    $new_con_num = $value; // 1 time loop (get value)
-                }
-
-                $con_num = $new_con_num;
+            print("Tesseract output: " . $scanned . "\n");
+            if (preg_match($regex_con_num, $scanned, $match)) {
+                $con_num = $match[0];
+                $scanned = str_replace($con_num, "", $scanned); //cut container number
+                print("scaned after found container: " . $scanned);
                 $container->container_number = $con_num;
-                $check_con++;
-                print($con_num . "\n");
+                print("\ncontainer number: " . $con_num);
             } else {
                 // not found container number
                 $con_num = "Not Found";
+                print("Container number: " . $con_num);
                 $container->container_number = $con_num;
             }
-            if (preg_match($regex_iso, $scanned, $match) && $check_iso == 0) {
-                foreach ($match as $key => $value) // 1 time loop (get value)
-                {
-                    $iso = $value;
+            if (preg_match($regex_iso, $scanned, $match)) {
+                $iso = $match[0];
+                $master_iso = MasterIso::where('code', '=', $iso)->get();
+                if (count($master_iso) == 1) { //real iso
+                    print("\niso: " . $iso);
+                    $container->iso = $iso;
+                } else {
+                    $iso = "Not Found";
+                    $container->iso = $iso;
+                    print("\niso: " . $iso);
+                    //not found iso
+                    for ($i = 0; $i < strlen($scanned) - 3; $i++) {
+                        //if begining 2 str start with number >> substr
+                        $str_4 = substr($scanned, $i, 4);
+                        print("\nsubstr: " . $str_4);
+                        if ((is_numeric($str_4[0]) || $str_4[0] == "/[BMPL]/") && is_numeric($str_4[1])) {
+                            $arr = $this->compareMasterIso($str_4);
+                            array_push($suggester_iso, $arr);
+                        }
+                    }
                 }
-                print($iso);
-                $container->iso = $iso;
-                $check_iso++;
             } else {
                 $iso = "Not Found";
                 $container->iso = $iso;
+                print("\niso: " . $iso);
                 //not found iso
-                // for ($i = 0; $i < strlen($scanned)-3; $i++) {
-                //     //if begining 2 str start with number >> substr
-                //     $str_4 = substr($scanned, $i, 4);
-                //     $this->compareMasterIso($str_4);
-                //     // print(" " . $str_4);
-                // }
-
+                for ($i = 0; $i < strlen($scanned) - 3; $i++) {
+                    //if begining 2 str start with number >> substr
+                    $str_4 = substr($scanned, $i, 4);
+                    print("\nsubstr: " . $str_4);
+                    if ((is_numeric($str_4[0]) || $str_4[0] == "/[BMPL]/") && is_numeric($str_4[1])) {
+                        $arr = $this->compareMasterIso($str_4);
+                        array_push($suggester_iso, $arr);
+                    }
+                }
             }
+
+            return response()->json($suggester_iso);
 
             $container->save();
         } catch (\Exception $e) {
@@ -129,10 +148,36 @@ class OcrController extends Controller
 
     private function compareMasterIso($str)
     {
-        $response = Http::get('https://datahub.io/core/iso-container-codes/r/iso-container-codes.json');
-        $master_iso = json_decode($response);
-        foreach (json_decode($response) as $master_iso) {
-            print_r("iso code: " . $master_iso->code . "\n"); //master_iso
-        }
+        print("\ncompare " . $str);
+        // $isos = MasterIso::get();
+
+        // foreach ($isos as $iso) {
+        //     print($iso->id . " " . $iso->code . "\n");
+        // }
+        // return $isos;
+
+        // $suggester_iso = MasterIso::where('code', 'LIKE', $str[0].$str[1]. "%")->get();
+        // $suggester_iso = MasterIso::where('code', 'LIKE', "%" .$str[1].$str[2]. "%")->get();
+        // $suggester_iso = MasterIso::where('code', 'LIKE', "%" .$str[2].$str[3])->get();
+        // $suggester_iso = MasterIso::where('code', 'LIKE', $str[0]. "%" .$str[3])->get();
+
+        $arr = [];
+
+        $suggester_iso1 = MasterIso::where('code', 'LIKE', $str[0] . $str[1] . "%" . $str[3])->get();
+
+        $suggester_iso2 = MasterIso::where('code', 'LIKE', "%" . $str[1] . $str[2] . $str[3])->get();
+
+        $suggester_iso3 = MasterIso::where('code', 'LIKE', $str[0] . "%" . $str[2] . $str[3])->get();
+
+        $suggester_iso4 = MasterIso::where('code', 'LIKE', $str[0] . $str[1] . $str[2] . "%")->get();
+
+        $merge = array_merge($suggester_iso1->toArray(), $suggester_iso2->toArray(), $suggester_iso3->toArray(), $suggester_iso4->toArray());
+        
+        return $merge;
+    }
+
+    public function test()
+    {
+        $this->compareMasterIso("L2G4");
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\MasterIso;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Http;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +22,18 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $response = Http::get('https://datahub.io/core/iso-container-codes/r/iso-container-codes.json');
+        $master_iso = json_decode($response);
+        foreach (json_decode($response) as $master_iso) {
+            $iso = new MasterIso();
+            $iso->code = $master_iso->code;
+            $iso->description = $master_iso->description;
+            $iso->group = $master_iso->group;
+            $iso->height = $master_iso->height;
+            $iso->length = $master_iso->length;
+            $iso->save();
+            // print_r("iso code: " . $master_iso->code . "\n"); //master_iso
+        } //loop put api to database
     }
 }
